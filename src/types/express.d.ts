d8fee2f2194306264a53199cafacf8c39c8cd084
MhKh <m.khoshnevis@arz.team>
2025-08-04 20:08:30 +0330

    feat(auth): hooking up auth

---
+++ src/types/express.d.ts
@@ -0,0 +1,14 @@
+import { User } from "@db/entities/user.entity";
+import { RedisService } from "@modules/shared/redis/redis.service";
+import { Request } from "express";
+import { RedisClientType, RediSearchSchema } from "redis"; // Or whatever type your redis client has
+
+// Augment the existing 'Request' interface
+declare global {
+  namespace Express {
+    export interface Request {
+      redis: RedisService;
+      user?: User;
+    }
+  }
+}
