9128f1fa39527f3942c52331eb01e1116b815358
MhKh <m.khoshnevis@arz.team>
2025-08-05 21:22:17 +0330

    cleanup: removing redundent method in repo

--- src/db/repositories/weather.repository.ts
+++ src/db/repositories/weather.repository.ts
@@ -4,20 +4,5 @@ import { Weather } from "../entities/weather.entity";
 import { City } from "@db/entities/city.entity";
 
 export const WeatherRepository = (dataSource: DataSource) => {
-  return dataSource.getRepository(Weather).extend({
-    async findFreshByCity(city: City, maxAgeMinutes = 15) {
-      const weather = await this.findOne({
-        where: { city },
-        order: { fetchedAt: "DESC" },
-      });
-
-      if (!weather) return null;
-
-      const now = new Date();
-      const isFresh =
-        now.getTime() - weather.fetchedAt.getTime() < maxAgeMinutes * 60 * 1000;
-
-      return isFresh ? weather : null;
-    },
-  });
+  return dataSource.getRepository(Weather);
 };
