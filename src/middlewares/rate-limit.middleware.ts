e3aaed11c036dbd82bb024525ed9b9feb2479717
MhKh <m.khoshnevis@arz.team>
2025-08-05 23:15:16 +0330

    fix(ratelimit): lowering limit

--- src/middlewares/rate-limit.middleware.ts
+++ src/middlewares/rate-limit.middleware.ts
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const apiRateLimiter = rateLimit({
-  windowMs: 5 * 60_000, // 15 minutes
-  max: 100, // Limit each IP to 200 requests per window
+  windowMs: 5 * 60_000, // 5 minutes
+  max: 400, // Limit each IP to 400 requests per window
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
   legacyHeaders: false, // Disable `X-RateLimit-*` headers
